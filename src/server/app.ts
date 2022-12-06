b85c67810f06e03d722efc173a3c74e6a27ee4e6
Henri Remonen <henri@remonen.fi>
2022-12-06 15:36:21 +0200

    Apply route for api/users to use usersRouter

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
+import usersRouter from './routes/users';
 
 require('express-async-errors');
 
@@ -10,13 +11,13 @@ app.use(express.json());
 // Routers goes in here
 //app.use('/api/login', loginRouter)
 //app.use('/api/blogs', userExtractor, blogsRouter)
-//app.use('/api/users', usersRouter)
+app.use('/api/users', usersRouter);
 //...etc
 
 if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing');
   app.use('/api/testing', testingRouter);
-}
+};
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
