a2e422029a283b44d3995265475b9105b7d8da98
Henri Remonen <henri@remonen.fi>
2022-12-06 16:12:07 +0200

    Create type for newly created users without certain fields

--- src/server/routes/users.ts
+++ src/server/routes/users.ts
@@ -2,11 +2,15 @@ import express from 'express'
 //import bcrypt from 'bcryptjs'
 //import UserModel from '../models/user'
 import { User } from "../types";
+import ValidateNormalUser from "../utils/usersValidator"
 
 const usersRouter = express.Router()
 
+export type NewUserFields = Omit<User, "usertype" | "applications" | "ecvs" >;
+
 usersRouter.post('/', async (request, response) => {
-  const newUser: User = await request.body;
+  const newUser: NewUserFields = ValidateNormalUser.parse(request.body)
+
   response.json(newUser)
 });
 
