f7446373067a3f36b4b3496cb64aab7d2101dd9a
Henri Remonen <henri@remonen.fi>
2022-12-06 15:22:35 +0200

    Create userSchema for normal user

---
+++ src/server/models/user.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import { User } from "../types";
+
+const userSchema = new mongoose.Schema<User>({
+  usertype: {type: String, required: true},
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true},
+  passwordHash: String,
+  birthdate: Date,
+  address: String,
+  ecvs: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Ecv'
+    }
+  ],
+  applications: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Application'
+    }
+  ]
+});
+
+userSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
