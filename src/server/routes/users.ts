50b4392b71a3a250b7c335042ba32c8de8469728
Henri Remonen <henri@remonen.fi>
2022-12-07 08:28:18 +0200

    UserRouter done

--- src/server/routes/users.ts
+++ src/server/routes/users.ts
@@ -1,17 +1,33 @@
-import express from 'express'
-//import bcrypt from 'bcryptjs'
-//import UserModel from '../models/user'
-import { User } from "../types";
-import ValidateNormalUser from "../utils/usersValidator"
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import { User, UserType } from "../types";
+import UserModel from '../models/user';
+import ValidateNormalUser from "../utils/usersValidator";
 
-const usersRouter = express.Router()
+const usersRouter = express.Router();
 
 export type NewUserFields = Omit<User, "usertype" | "applications" | "ecvs" >;
 
 usersRouter.post('/', async (request, response) => {
-  const newUser: NewUserFields = ValidateNormalUser.parse(request.body)
+  const newUser: NewUserFields = ValidateNormalUser.parse(request.body);
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(newUser.password, saltRounds); 
 
-  response.json(newUser)
+  const user = new UserModel({
+    usertype: UserType.NormalUser,
+    firstName: newUser.firstName,
+    lastName: newUser.lastName,
+    email: newUser.email,
+    password: passwordHash,
+    phoneNumber: newUser.phoneNumber ?? "",
+    address: newUser.address ?? "",
+    ecvs: [],
+    applications: []
+  });
+
+  const savedUser = await user.save();
+
+  response.status(201).json(savedUser);
 });
 
 export default usersRouter;
