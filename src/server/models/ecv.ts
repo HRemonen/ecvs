1fdfdf6ca54e9bdeff21924637f5399b7c4cb20a
Henri Remonen <henri@remonen.fi>
2022-12-07 11:33:49 +0200

    Create Ecv database schema

--- src/server/models/ecv.ts
+++ src/server/models/ecv.ts
@@ -1,2 +1,31 @@
 import mongoose from "mongoose";
+import { Ecv } from "../types";
 
+const ecvSchema = new mongoose.Schema<Ecv>({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  expertise: [String],
+  skills: [String],
+  education: [Object],
+  experience: [Object],
+  qualifications: [String],
+  hobbies: [String],
+  languages: [String],
+  references: [String],
+  socials: [String],
+  profile: String
+});
+
+ecvSchema.set('toJSON', {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  }
+});
+
+const EcvModel = mongoose.model('Ecv', ecvSchema);
+
+export default EcvModel;
