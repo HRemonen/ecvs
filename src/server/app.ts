e8d451458fdad45ca001c2496e95b0b1c3988a97
Henri Remonen <henri@remonen.fi>
2022-12-07 09:05:20 +0200

    Add mongodb connection

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import cors from 'cors';
 import usersRouter from './routes/users';
+import mongoose from "mongoose";
+import config from './utils/config';
 
 require('express-async-errors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+void mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
 
 // Routers goes in here
 //app.use('/api/login', loginRouter)
