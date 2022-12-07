3bd529b32331e7a90a75aaa28d59a410686d1023
Henri Remonen <henri@remonen.fi>
2022-12-07 09:05:04 +0200

    When no database is given application shuts down

--- src/server/utils/config.ts
+++ src/server/utils/config.ts
@@ -1,8 +1,13 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT
-const MONGODB_URI = process.env.NODE_ENV === 'test'
+const MONGODB_URI: string | undefined = process.env.NODE_ENV === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
+
+if (!MONGODB_URI) {
+  console.log("Cannot start process with out database URL");
+  process.exit()
+};
 
 export default { PORT, MONGODB_URI };
