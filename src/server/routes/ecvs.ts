0fbbf72da98aa04230cc84a23368ecb1bc13a53b
Henri Remonen <henri@remonen.fi>
2022-12-10 15:42:48 +0200

    Refactored ecvs API

--- src/server/routes/ecvs.ts
+++ src/server/routes/ecvs.ts
@@ -1,4 +1,4 @@
-import express, { Response } from 'express';
+import express from 'express';
 import EcvModel from '../models/ecv';
 import EcvZod from '../utils/ecvsValidator';
 import ecvsService from '../services/ecvsService';
@@ -6,21 +6,6 @@ import { userExtractor, CustomRequest } from '../middlewares/middleware';
 
 
 const ecvsRouter = express.Router();
-
-const getEcvById = async (ecvId: string, response: Response) => {
-  const ecv = await EcvModel.findById(ecvId);
-  if (!ecv) {
-    return response.status(401).json({ error: 'malformatted id'});
-  }
-  return ecv;
-};
-
-const checkAuthorization = (user: string, ecv: any) => {
-  if (!user || ecv.user.toString() !== user) {
-    return false;
-  }
-  return true;
-};
 
 ecvsRouter.get('/', async (_request, response) => {
   const ecvs = await ecvsService.getEcvs();
@@ -39,49 +24,48 @@ ecvsRouter.post('/', userExtractor, async (request, response) => {
   if (!parsedEcv.success) {
     return response.status(400).json(parsedEcv.error);
   }
-
   const savedEcv = await ecvsService.createEcv(parsedEcv.data);
 
   return response.status(201).json(savedEcv);
 });
 
 ecvsRouter.put('/:id', userExtractor, async (request, response) => {
-  const ecvToUpdate = await getEcvById(request.params.id, response);
+  const ecvToUpdate = await EcvModel.findById(request.params.id);
   const user = (request as CustomRequest).user;
 
-  if (!user) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+  if (!ecvToUpdate) {
+    return response.status(401).json({ error: 'malformatted id'});
   }
-
   if (!request.body || request.body.length === 0) {
     return response.status(401).json({ error: "updated fields missing" });
   }
-
-  if (checkAuthorization(user, ecvToUpdate)) {
-    const parsedEcv = EcvZod.safeParse({ ...request.body, user});
-
-    if (!parsedEcv.success) {
-      return response.status(400).json(parsedEcv.error);
-    }
-    const updatedEcv = await ecvsService.updateEcv(request.params.id, parsedEcv.data);
-    return response.status(201).json(updatedEcv);
+  if (!user || ecvToUpdate.user.toString() !== user) {
+    return response.status(401).json({ error: 'unauthorized action' });
   }
-  return response.status(401).json({ error: 'unauthorized action' });
+  
+  const parsedEcv = EcvZod.safeParse({ ...request.body, user});
+
+  if (!parsedEcv.success) {
+    return response.status(400).json(parsedEcv.error);
+  }
+  const updatedEcv = await ecvsService.updateEcv(request.params.id, parsedEcv.data);
+  return response.status(201).json(updatedEcv);
 });
 
 ecvsRouter.delete('/:id', userExtractor, async (request, response) => {
-  const ecvToDelete = await getEcvById(request.params.id, response);
+  const ecvToDelete = await EcvModel.findById(request.params.id);
   const user = (request as CustomRequest).user;
 
-  if (!user) {
-    return response.status(401).json({ error: 'token missing or invalid' })
+  if (!ecvToDelete) {
+    return response.status(401).json({ error: 'malformatted id'});
+  }
+  if (!user || ecvToDelete.user.toString() !== user) {
+    return response.status(401).json({ error: 'unauthorized action' });
   }
 
-  if (checkAuthorization(user, ecvToDelete)) {
-    await ecvsService.deleteEcv(request.params.id, user);
-    return response.status(204).end();
-  }
-  return response.status(401).json({ error: 'unauthorized action' });
+  await ecvsService.deleteEcv(ecvToDelete.id, user);
+
+  return response.status(204).end();
 });
 
 export default ecvsRouter;
