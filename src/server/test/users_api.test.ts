20679ca59620b89887103ba24795f690c2cbde4b
Henri Remonen <henri@remonen.fi>
2022-12-10 10:28:57 +0200

    Usercreation tests complete

--- src/server/test/users_api.test.ts
+++ src/server/test/users_api.test.ts
@@ -1,8 +1,9 @@
 import app from "../app";
+import bcrypt from 'bcryptjs';
 import mongoose from "mongoose";
 import supertest from "supertest";
 
-import { User } from "../types";
+import { User, UserType } from "../types";
 import UserModel from "../models/user";
 import getTestUsers from "./data/users";
 
@@ -22,7 +23,7 @@ describe('Database returns JSON content', () => {
   });
 });
 
-describe('Users', () => {
+describe('users in database', () => {
 
   beforeEach(async () => {
     await UserModel.deleteMany({});
@@ -44,6 +45,15 @@ describe('Users', () => {
     expect(response.body[1].lastName).toEqual(testUsers[1].lastName);
     expect(response.body[1].address).toEqual(testUsers[1].address);
   });
+
+  test('users are identified with an id', async () => {
+    const response = await api
+      .get(USER_API)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+
+      expect(response.body[0].id).toBeDefined();
+  })
 
   test('a valid user can be created to the database', async () => {
     const newUser = {
@@ -84,24 +94,160 @@ describe('Users', () => {
 
       expect(response.body).toHaveLength(testUsers.length);
   });
+});
 
-  test('user with already existing email cannot be created to the database', async () => {
+describe('user creations', () => {
+  beforeEach(async () => {
+    await UserModel.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+
+    const user = new UserModel({
+      usertype: UserType.NormalUser,
+      firstName: "Kalevi",
+      lastName: "Kalvirinne",
+      email: "k.kalvirinne@gmail.com",
+      password: passwordHash,
+      phoneNumber: "123-123-1234",
+      address: "Kalevinrinne 60",
+      ecvs: [],
+      applications: []
+    });
+
+    await user.save();
+  });
+
+  test('fails if firstname is too short', async () => {
     const newUser = {
-      firstName: "Nakki",
-      lastName: "Nakkila",
+      firstName: "a",
+      lastName: "Testi",
+      email: "aasdf@abc.com",
+      password: "salainen",
+    };
+
+    const response = await api
+      .post(USER_API)
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const issues = response.body.issues[0];
+
+    expect(issues).toMatchObject({"message": "Firstname must be 2 or more characters long"});
+  });
+
+  test('fails if firstname is too long', async () => {
+    const newUser = {
+      firstName: "TestimiesTestimiesTestimies",
+      lastName: "Testi",
+      email: "aasdf@abc.com",
+      password: "salainen",
+    };
+
+    const response = await api
+      .post(USER_API)
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const issues = response.body.issues[0];
+
+    expect(issues).toMatchObject({"message": "Firstname must be 18 or less characters long"});
+  });
+
+  test('fails if lastname is too short', async () => {
+    const newUser = {
+      firstName: "Testi",
+      lastName: "t",
+      email: "aasdf@abc.com",
+      password: "salainen",
+    };
+
+    const response = await api
+      .post(USER_API)
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const issues = response.body.issues[0];
+
+    expect(issues).toMatchObject({"message": "Lastname must be 2 or more characters long"});
+  });
+
+  test('fails if lastname is too long', async () => {
+    const newUser = {
+      firstName: "Testi",
+      lastName: "TestimiesTestimiesTestimies",
+      email: "aasdf@abc.com",
+      password: "salainen",
+    };
+
+    const response = await api
+      .post(USER_API)
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const issues = response.body.issues[0];
+
+    expect(issues).toMatchObject({"message": "Lastname must be 18 or less characters long"});
+  });
+
+  test('fails with proper statuscode if email already in use', async () => {
+    const newUser = {
+      firstName: "test",
+      lastName: "Testi",
       email: "k.kalvirinne@gmail.com",
       password: "salainen",
     };
 
-    await api
+    const response = await api
       .post(USER_API)
       .send(newUser)
       .expect(400)
+      .expect('Content-Type', /application\/json/);
 
-    const response = await api.get(USER_API);
-      
-    expect(response.body).toHaveLength(testUsers.length);
+    expect(response.body.error).toContain("Email already in use by another user");
   });
+
+  test('fails with proper statuscode if malformatted email', async () => {
+    const newUser = {
+      firstName: "test",
+      lastName: "Testi",
+      email: "kalvi(at)kalvi.fi",
+      password: "salainen",
+    };
+
+    const response = await api
+      .post(USER_API)
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const issues = response.body.issues[0];
+
+    expect(issues).toMatchObject({"message": "Invalid email"});
+  });
+
+  test('fails if password is too short', async () => {
+    const newUser = {
+      firstName: "Testi",
+      lastName: "Testi",
+      email: "testi@abc.com",
+      password: "a",
+    };
+
+    const response = await api
+      .post(USER_API)
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/);
+
+    const issues = response.body.issues[0];
+
+    expect(issues).toMatchObject({"message": "Password must be 8 or more characters long"});
+  });
+
 });
 
 afterAll(() => {mongoose.connection.close()});
--- src/server/utils/usersValidator.ts
+++ src/server/utils/usersValidator.ts
@@ -6,10 +6,14 @@ export const LoginZod = z.object({
 });
 
 export const UserZod = z.object({
-  firstName: z.string().min(2).max(18),
-  lastName: z.string().min(2).max(18),
+  firstName: z.string()
+    .min(2, { message: "Firstname must be 2 or more characters long" })
+    .max(18, { message: "Firstname must be 18 or less characters long" }),
+  lastName: z.string()
+    .min(2, { message: "Lastname must be 2 or more characters long" })
+    .max(18, { message: "Lastname must be 18 or less characters long" }),
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z.string().min(8, { message: "Password must be 8 or more characters long" }),
   phoneNumber: z.string().min(10).optional(),
   address: z.string().optional(),
 });
