8f5363bc52ae6c0c7a82263da1609f52e1e1db96
Henri Remonen <henri@remonen.fi>
2022-12-10 11:30:04 +0200

    Ecvs tests complete

--- src/server/test/ecvs_api.test.ts
+++ src/server/test/ecvs_api.test.ts
@@ -1,13 +1,17 @@
 import app from "../app";
+import bcrypt from 'bcryptjs';
 import mongoose from "mongoose";
 import supertest from "supertest";
 
 import getEcvsData from "./data/ecvs";
 import EcvModel from "../models/ecv";
+import UserModel from "../models/user";
+import { UserType } from "../types";
 
 const api = supertest(app);
 
 const ECVS_API = "/api/ecvs";
+const LOGIN_API = "/api/login"
 
 describe('Database returns JSON content', () => {
 
@@ -19,7 +23,7 @@ describe('Database returns JSON content', () => {
   });
 });
 
-describe('users in database', () => {
+describe('ecvs in database', () => {
 
   beforeEach(async () => {
     await EcvModel.deleteMany({});
@@ -36,11 +40,8 @@ describe('users in database', () => {
   test('test ecvs have an user linked', async () => {
     const response = await api.get(ECVS_API);
 
-    expect(response.body[0].user).toBeDefined();
-    expect(response.body[1].user).toBeDefined();
-
-    expect(response.body[0].user).toHaveProperty("id")
-    expect(response.body[1].user).toHaveProperty("id")
+    expect(response.body[0]).toHaveProperty("user");
+    expect(response.body[1]).toHaveProperty("user");
   });
 
   test('ecvs are identified with an id', async () => {
@@ -49,9 +50,67 @@ describe('users in database', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/);
 
-    expect(response.body[0].id).toBeDefined();
-    expect(response.body[1].id).toBeDefined();
+    expect(response.body[0]).toBeDefined();
+    expect(response.body[1]).toBeDefined();
+    expect(response.body[0]).toHaveProperty("id")
+    expect(response.body[1]).toHaveProperty("id")
   })
+});
+
+describe('ecv creation', () => {
+  let token: string;
+
+  beforeEach(async () => {
+    await UserModel.deleteMany({});
+
+    const passwordHash = await bcrypt.hash('todellasalainen', 10)
+
+    const user = new UserModel({
+      usertype: UserType.NormalUser,
+      firstName: "Ecvs",
+      lastName: "Testi",
+      email: "ecvs@testi.fi",
+      password: passwordHash,
+      phoneNumber: "123-123-1234",
+      address: "Testirinne 2",
+      ecvs: [],
+      applications: []
+    });
+
+    await user.save();
+
+    const response = await api
+      .post(LOGIN_API)
+      .send({
+        email: "ecvs@testi.fi",
+        password: "todellasalainen"
+      });
+
+    token = response.body.token;
+  });
+
+  test('succeeds if valid ecvs and token', async () => {
+    await api
+      .post(ECVS_API)
+      .send({
+        profile: "olen hyvä koodaaja"
+      })
+      .set('Authorization', `bearer ${token}`)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  });
+
+  test('fails if valid ecvs and invalid token', async () => {
+    const response = await api
+      .post(ECVS_API)
+      .send({
+        profile: "olen hyvä koodaaja"
+      })
+      .expect(401)
+      .expect('Content-Type', /application\/json/)
+
+    expect(response.body.error).toContain("token missing or invalid");
+  });
 });
 
 afterAll(() => {mongoose.connection.close()});
