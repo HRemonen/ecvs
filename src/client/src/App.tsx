b952e906ccb6c8f2d15685a468a8b4c4e177a24f
Henri Remonen <henri@remonen.fi>
2022-12-12 11:19:51 +0200

    Initialize frontend

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "./hooks/dispatchHooks";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { initializeEcvs } from "./reducers/ecvReducer";
 
+const App = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(initializeEcvs());
+  }, [dispatch]);
+
+  const ecvs = useAppSelector(state => state.ecvs)
+  console.log(ecvs);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
