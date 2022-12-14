0806f662662b89d64592b3dcff4de61f814c998e
Henri Remonen <henri@remonen.fi>
2022-12-14 10:25:59 +0200

    Add userReducer to the store

--- src/client/src/store.ts
+++ src/client/src/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import ecvReducer from "./reducers/ecvReducer";
 import authReducer from "./reducers/authReducer";
+import userReducer from "./reducers/userReducer";
 
 export const store = configureStore({
   reducer: {
     ecvs: ecvReducer,
+    users: userReducer,
     authentication: authReducer,
   },
 });
