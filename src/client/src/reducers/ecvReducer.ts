ba7dbea233afe625b46df438f691cd6d6d12f5b6
Henri Remonen <henri@remonen.fi>
2022-12-17 10:44:51 +0200

    Create action creators for removing and creating ecvs

--- src/client/src/reducers/ecvReducer.ts
+++ src/client/src/reducers/ecvReducer.ts
@@ -1,6 +1,9 @@
 import { createSlice, Dispatch } from "@reduxjs/toolkit";
+
 import ecvsService from "../services/ecvsService";
-import { Ecv } from '@backend/types';
+
+import type { Ecv } from '@backend/types';
+import type { ValidatedEcv } from "@backend/utils/ecvsValidator";
 
 const initialState: Array<Ecv & {id: string}> = [];
 
@@ -10,16 +13,36 @@ const ecvsSlice = createSlice({
   reducers: {
     setEcvs(state, action) {
       return action.payload;
+    },
+    append(state, action) {
+      state.push(action.payload);
+    },
+    remove(state, action) {
+      return state.filter(ecv => ecv.id === action.payload);
     }
   }
 });
 
-export const { setEcvs } = ecvsSlice.actions;
+export const { setEcvs, append, remove } = ecvsSlice.actions;
 
 export const initializeEcvs = () => {
   return async (dispatch: Dispatch): Promise <void> => {
     const ecvs = await ecvsService.getEcvs();
     dispatch(setEcvs(ecvs));
+  };
+};
+
+export const createEcv = (ecv: ValidatedEcv) => {
+  return async (dispatch: Dispatch): Promise <void> => {
+    const newEcv = await ecvsService.createEcv(ecv);
+    dispatch(append(newEcv));
+  };
+};
+
+export const deleteEcv = (ecv: Ecv & {id: string}) => {
+  return async (dispatch: Dispatch): Promise <void> => {
+    await ecvsService.removeEcv(ecv.id)
+    dispatch(remove(ecv.id));
   };
 };
 
