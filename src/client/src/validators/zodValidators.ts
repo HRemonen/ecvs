f87025961605f9afd7187e174252ab08b0d00cdf
Henri Remonen <henri@remonen.fi>
2022-12-19 14:24:48 +0200

    Add new User zod to the validators from backend

--- src/client/src/validators/zodValidators.ts
+++ src/client/src/validators/zodValidators.ts
@@ -5,6 +5,19 @@ export const LoginZod = z.object({
   password: z.string().min(8, { message: "Password must be 8 or more characters long" })
 });
 
+export const UserZod = z.object({
+  firstName: z.string()
+    .min(2, { message: "Firstname must be 2 or more characters long" })
+    .max(18, { message: "Firstname must be 18 or less characters long" }),
+  lastName: z.string()
+    .min(2, { message: "Lastname must be 2 or more characters long" })
+    .max(18, { message: "Lastname must be 18 or less characters long" }),
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Password must be 8 or more characters long" }),
+  phoneNumber: z.string().min(10).optional(),
+  address: z.string().optional(),
+});
+
 export const EcvZod = z.object({
   skills: z.array(z.string()).optional(),
   education: z.array(z.object({
