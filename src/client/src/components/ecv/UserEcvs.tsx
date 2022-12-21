0d0f72bda12a25fcea4ddc58ff780949bf94f2d7
Henri Remonen <henri@remonen.fi>
2022-12-21 08:54:46 +0200

    Fix issue where all the ecvs was rendered regardless of the authenticated user

--- src/client/src/components/ecv/UserEcvs.tsx
+++ src/client/src/components/ecv/UserEcvs.tsx
@@ -17,7 +17,7 @@ const UserEcvs: React.FC<{user: User & {id: string}}> = ({ user }) => {
   return (
     <div className="md:ml-2">
       <div className="grid gap-2 md:gap-4 md:grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3">
-        {ecvs.map(e => (
+        {userEcvs.map(e => (
           <EcvCard key={e.id} ecv={e} />
         ))}
       </div>
