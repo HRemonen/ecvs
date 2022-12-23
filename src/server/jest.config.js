1d577207bc9ace71cf8ac82cc5d6c20461be76cb
Henri Remonen <henri@remonen.fi>
2022-12-23 11:32:49 +0200

    Update jest config to exclude unneccesary

--- src/server/jest.config.js
+++ src/server/jest.config.js
@@ -1,5 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
+// eslint-disable-next-line no-undef
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  testEnvironment: 'node',
+  "collectCoverage": true,
+    "coveragePathIgnorePatterns": [
+      "build",
+      "dist",
+      "utils",
+      "data",
+      "testing.ts"
+  ]
 };
