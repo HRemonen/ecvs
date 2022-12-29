af28f598c5a0ba83e8876d1844ea191e51a68e90
Henri Remonen <henri@remonen.fi>
2022-12-29 19:02:29 +0200

    Styling

--- src/client/src/components/ecv/EcvCard.tsx
+++ src/client/src/components/ecv/EcvCard.tsx
@@ -11,25 +11,25 @@ const EcvCard: React.FC<{ecv: Ecv & {id: string}}> = ({ ecv }) => {
   const dispatch = useAppDispatch();
 
   return (
-    <div className="grid grid-cols-1 max-w-4xl px-10 my-4 py-6 bg-white rounded-lg shadow-md">
-      <div className="flex justify-between items-center">
-        <h5 className="font-light text-gray-600">
-          Ecv # { ecv.id }
-        </h5>
-        <div>
-          <DeleteButton onClick={() => dispatch(deleteEcv(ecv))} />
+    <div className="p-4">
+      <div className="grow border border-gray-400 md:border-gray-400 bg-white rounded p-4 flex flex-col justify-between leading-normal">
+        <div className="flex justify-between items-center">
+          <h5 className="font-light text-gray-600">
+            Ecv # { ecv.id }
+          </h5>
+          <div>
+            <DeleteButton onClick={() => dispatch(deleteEcv(ecv))} />
+          </div>
         </div>
-      </div>
-      
-      <div className="">
-        <ul className="">
-          <li className="">
-            { Object.entries(ecv).map(([key, value]) => (
-              <EcvFields key={key} field={key} content={value}/>
-            ))}
-          </li>
-        </ul>
-        
+        <div>
+          <ul>
+            <li>
+              { Object.entries(ecv).map(([key, value]) => (
+                <EcvFields key={key} field={key} content={value}/>
+              ))}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   )
