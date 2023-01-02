5aa7e648e315cf32a311845f47ec526ad0fcb978
Henri Remonen <henri@remonen.fi>
2023-01-02 14:10:18 +0200

    Create command for creating ecv

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 /// <reference types="cypress" />
+
 // ***********************************************
 // This example commands.ts shows you how to
 // create various custom commands and overwrite
@@ -21,6 +22,34 @@ Cypress.Commands.add('login', (email, password) => {
     cy.visit('http://localhost:3001')
   })
 })
+
+Cypress.Commands.add('createEcv', () => {
+  const token = localStorage.getItem('loggedUser')
+  const options = {
+    method: 'POST',
+    url: 'http://localhost:3001/api/ecvs',
+    body: {
+    experience: [{
+      company: "Testingcompany",
+      position: "tester man",
+      startDate: new Date(2008, 1, 1)
+    }],
+    education: [{
+      school: "MIT",
+      startDate: new Date(2019, 8, 1)
+    }],
+    skills: ['Coding', 'Testing', 'E2e', 'QA'],
+    hobbies: ['Rowing'],
+    languages: ['Finnish'],
+    Profile: "Testerman yuh"
+  },
+  headers: {
+    'Authorization': `bearer ${JSON.parse(token).token}`
+  }
+}
+cy.request(options)
+
+})
 //
 //
 // -- This is a child command --
@@ -37,7 +66,8 @@ Cypress.Commands.add('login', (email, password) => {
 declare global {
   namespace Cypress {
     interface Chainable {
-      login(email: string, password: string): Chainable<void>
+      login(email: string, password: string): Chainable<void>,
+      createEcv(): Chainable<void>
 //       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
 //       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
 //       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
