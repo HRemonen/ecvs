0d69a9ad5427bf7bd6691011924d045ca6dead29
Henri Remonen <henri@remonen.fi>
2023-01-02 14:10:00 +0200

    Create tests for authenticated user posting view

--- cypress/e2e/ecves_applying_process.cy.ts
+++ cypress/e2e/ecves_applying_process.cy.ts
@@ -16,7 +16,7 @@ describe('Ecves postings', function () {
   })
 })
 
-describe('Authenticated user application process', function () {
+describe('Authenticated user posting page', function () {
   before(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
   
@@ -28,7 +28,34 @@ describe('Authenticated user application process', function () {
     }
   
     cy.request('POST', 'http://localhost:3001/api/users', user)
-    cy.visit('http://localhost:3001/')
   })
 
+  beforeEach(function () {
+    cy.login("testi@testi.fi", "salasana")
+    cy.visit('http://localhost:3001/postings')
+  })
+
+  it('user can view postings', function () {
+    cy.visit('http://localhost:3001/')
+    cy.contains('Postings').click()
+
+    cy.get('#posting-list').children().should('not.have.length', 0)
+    cy.get('ul#posting-list').children().should('contain', 'More info')
+  })
+
+  it('user can filter postings', function () {
+    cy.visit('http://localhost:3001/postings')
+    cy.get('#search').type('Developer{enter}')
+
+    cy.get('#posting-list').children().should('contain', 'Frontend developer')
+    cy.get('ul#posting-list').children().should('contain', 'More info')
+  })
+
+  it('user can visit single posting page', function () {
+    cy.contains('More info').click()
+    cy.contains('Ends on')
+    cy.contains('Back to postings')
+    cy.contains('More info')
+    cy.contains('You dont have any Ecvs to apply with.')
+  })
 })
