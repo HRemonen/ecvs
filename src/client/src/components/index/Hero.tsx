323efcfba6afb432c2f0eff24636a3a5d30e32b2
Henri Remonen <henri@remonen.fi>
2023-01-06 12:36:43 +0200

    Hero counters takes the lenght of states

--- src/client/src/components/index/Hero.tsx
+++ src/client/src/components/index/Hero.tsx
@@ -1,4 +1,5 @@
 import { useSpring, animated } from "@react-spring/web";
+import { useAppSelector } from "../../hooks/dispatchHooks";
 import Navbar from "./Navbar";
 
 const NumberAnimation: React.FC<{n: number}> = ({ n }) => {
@@ -11,39 +12,45 @@ const NumberAnimation: React.FC<{n: number}> = ({ n }) => {
   return <animated.div>{ number.to((n) => n.toFixed(0))}</animated.div>
 }
 
-const Hero = () => (
-  <div className="snap-start w-full h-screen">
-    <Navbar />
-    <div className="pt-8 md:pt-48 md:max-w-[800px] w-4/5 mx-auto text-center flex flex-col justify-center">
-      <div className="">
-        <p className="text-2xl font-bold pb-2">
-          Your dream job is just a click away
-        </p>
-        <h1 className="pt-8 bg-gradient-to-r from-purple-600 to-blue-500 bg-clip-text text-5xl font-extrabold text-transparent sm:text-5xl">
-          From resumes to interviews, we&lsquo;ve got you covered
-        </h1>
-        <p className="hidden md:block pt-4">
-          Our platform connects job seekers and employers in one easy-to-use web platform. 
-          Post your CV and search for job postings, or post job openings and search for qualified candidates. 
-          Our platform makes it easy to find your next career opportunity.
-        </p>
+const Hero = () => {
+  const users = useAppSelector(state => state.users);
+  const ecvs = useAppSelector(state => state.ecvs);
+  const postings = useAppSelector(state => state.postings);
+
+  return (
+    <div className="snap-start w-full h-screen">
+      <Navbar />
+      <div className="pt-8 md:pt-48 md:max-w-[800px] w-4/5 mx-auto text-center flex flex-col justify-center">
+        <div className="">
+          <p className="text-2xl font-bold pb-2">
+            Your dream job is just a click away
+          </p>
+          <h1 className="pt-8 bg-gradient-to-r from-purple-600 to-blue-500 bg-clip-text text-5xl font-extrabold text-transparent sm:text-5xl">
+            From resumes to interviews, we&lsquo;ve got you covered
+          </h1>
+          <p className="hidden md:block pt-4">
+            Our platform connects job seekers and employers in one easy-to-use web platform. 
+            Post your CV and search for job postings, or post job openings and search for qualified candidates. 
+            Our platform makes it easy to find your next career opportunity.
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-between items-center pt-16 md:pt-96 md:mx-48 text-center md:text-6xl font-light">
+        <div>
+          <NumberAnimation n={users.length} />
+          <p className="pt-8 font-thin md:text-lg">Users on platform</p>
+        </div>
+        <div>
+          <NumberAnimation n={ecvs.length} />
+          <p className="pt-8 font-thin md:text-lg">Ecvs created</p>
+        </div>
+        <div>
+          <NumberAnimation n={postings.length} />
+          <p className="pt-8 font-thin md:text-lg">Job opportunities</p>
+        </div>
       </div>
     </div>
-    <div className="flex justify-between items-center pt-16 md:pt-96 md:mx-48 text-center md:text-6xl font-light">
-      <div>
-        <NumberAnimation n={3} />
-        <p className="pt-8 font-thin md:text-lg">Users on platform</p>
-      </div>
-      <div>
-        <NumberAnimation n={21} />
-        <p className="pt-8 font-thin md:text-lg">Ecvs created</p>
-      </div>
-      <div>
-        <NumberAnimation n={103} />
-        <p className="pt-8 font-thin md:text-lg">Job opportunities</p>
-      </div>
-    </div>
-  </div>
-);
+  )
+};
 
 export default Hero;
