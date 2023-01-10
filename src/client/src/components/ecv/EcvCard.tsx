71940b3a9271c4aea7d492cf382124b20b217d8f
Henri Remonen <henri@remonen.fi>
2023-01-10 13:59:17 +0200

    Style Ecv renaming input field

--- src/client/src/components/ecv/EcvCard.tsx
+++ src/client/src/components/ecv/EcvCard.tsx
@@ -5,6 +5,7 @@ import { updateEcv, deleteEcv } from "../../reducers/ecvReducer";
 
 import DeleteButton from "../misc/Buttons";
 import EcvFields from "./EcvFields";
+import { MdOutlineSyncProblem } from "react-icons/md";
 
 import type { Ecv } from "@backend/types";
 
@@ -31,17 +32,25 @@ const EcvCard: React.FC<{ecv: Ecv & {id: string}}> = ({ ecv }) => {
       <div className="grow border border-gray-400 md:border-gray-400 bg-white rounded p-4 flex flex-col justify-between leading-normal">
         <div className="flex justify-between items-center">
           { editMode
-            ? <div>
-                <input type="text" defaultValue={name} onChange={(e) => setNewName(e.target.value)}/>
-                <button onClick={() => setEditMode(!editMode)}>Cancel</button>
-                <button onClick={handleNameChange}>OK </button>
+            ? <div className="flex ">
+                <span className="inline-flex items-center px-2 text-sm text-gray-900 bg-gray-200 border border-r-0 border-gray-300 rounded-l-md ">
+                  <MdOutlineSyncProblem size={24}/>
+                </span>
+                <input type="text" 
+                  className="rounded-none rounded-r-lg bg-gray-50 border text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm border-gray-300 p-1" 
+                  defaultValue={name} onChange={(e) => setNewName(e.target.value)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") handleNameChange()
+                  }}
+                />
+                <button className="ml-4" onClick={() => setEditMode(!editMode)}>Cancel</button>
               </div>
             : <h5 className="font-light text-gray-600" onDoubleClick={() => setEditMode(!editMode)}>
                 { name }
               </h5>
           }
           <div>
-            <h1>{ JSON.stringify(ecv.createdOn) }</h1>
+            <h1 className="font-light text-gray-600">{ new Date(ecv.createdOn).toDateString() }</h1>
           </div>
           <div>
             <DeleteButton onClick={() => dispatch(deleteEcv(ecv))} />
